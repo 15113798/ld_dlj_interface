c99b942c09e40c7c4244efe816edc5878dc9d950
15113798@qq.com <15113798@qq.com>
2020-04-18 01:36:34 +0800

    no message

--- src/main/java/io/renren/modules/pc/controller/HighConController.java
+++ src/main/java/io/renren/modules/pc/controller/HighConController.java
@@ -2,28 +2,28 @@ package io.renren.modules.pc.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.renren.common.utils.DateUtils;
-import io.renren.common.utils.Query;
 import io.renren.common.utils.R;
 import io.renren.modules.generator.entity.DljIndustryDataEntity;
 import io.renren.modules.generator.entity.DljIndustryEntity;
-import io.renren.modules.generator.entity.DljIndustryMenuEntity;
 import io.renren.modules.generator.service.DljIndustryDataService;
-import io.renren.modules.generator.service.DljIndustryMenuService;
 import io.renren.modules.generator.service.DljIndustryService;
 import io.renren.modules.pc.entity.EchartData;
 import io.renren.modules.pc.entity.LastYearEleConEntity;
 import io.renren.modules.pc.entity.Series;
 import io.renren.modules.pc.service.IndClassService;
-import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("industryClass")
-public class IndustryClassController {
+@RequestMapping("highCon")
+public class HighConController {
     @Autowired
     private DljIndustryDataService service;
     @Autowired
@@ -36,37 +36,67 @@ public class IndustryClassController {
 
     @RequestMapping("/getData")
     public R getData(@RequestParam Map<String, Object> params){
-        String  industryId = String.valueOf(params.get("industryId"));
 
-        //数据统一是以查上月为准
-        String DATE_PATTERN = "yyyy-MM-dd";
-        String lastMonth = DateUtils.getLastMonth();
-        //通过行业id获取行业名称
-        QueryWrapper wrapper = new QueryWrapper();
-        wrapper.eq("id",industryId);
-        List<DljIndustryEntity> list= industryService.list(wrapper);
-        DljIndustryEntity indEntity = list.get(0);
-
-        //通过行业和时间去获取准确的一条消息
-        QueryWrapper dataWrapper = new QueryWrapper();
-        dataWrapper.eq("record_time",lastMonth);
-        dataWrapper.eq("industry_name",indEntity.getOverName());
-        List<DljIndustryDataEntity> dataList = service.list(dataWrapper);
-        DljIndustryDataEntity dataEntity =  dataList.get(0);
-        return R.ok().put("data", dataEntity);
+        return R.ok().put("data", null);
     }
 
 
 
     @RequestMapping("/getDisData")
     public R getDisData(@RequestParam Map<String, Object> params){
-        String  timeType = String.valueOf(params.get("timeType"));
-        if("1".equals(timeType)){
-            return R.ok().put("data",indClassService.getDisDataByMonth(params));
-        }else if("2".equals(timeType)){
-            return R.ok().put("data",indClassService.getDisDataByYear(params));
-        }
+        String  industryId = String.valueOf(params.get("industryId"));
+        String  type = String.valueOf(params.get("type"));
+        //通过行业id获取行业名称
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id",industryId);
+        List<DljIndustryEntity> list= industryService.list(wrapper);
+        DljIndustryEntity indEntity = list.get(0);
 
+        //通过行业去获取所有的list，然后获取到list拼接折线图的数据
+        QueryWrapper dataWrapper = new QueryWrapper();
+        dataWrapper.eq("industry_name",indEntity.getOverName());
+        List<DljIndustryDataEntity> dataList = service.list(dataWrapper);
+        if(dataList != null){
+            String typeName = "";
+
+            if(type.equals("1")){
+                typeName = "用户量";
+            }else if(type.equals("2")){
+                typeName = "装机容量";
+            }else if(type.equals("3")){
+                typeName = "电量";
+            }else{
+                typeName = "产能利用率";
+            }
+
+            //分组
+            List<String> legend = new ArrayList<String>();
+            legend.add(typeName);
+
+            List<String> category= new ArrayList<>();
+            List<String> dataSerList = new ArrayList<>();
+            for (DljIndustryDataEntity entity : dataList) {
+                category.add(entity.getRecordTime());
+                String dataStr = "";
+                if(type.equals("1")){
+                    dataStr = entity.getUserNum();
+                }else if(type.equals("2")){
+                    dataStr = entity.getInstalledCapacity();
+                }else if(type.equals("3")){
+                    dataStr = entity.getEleConMonth();
+                }else{
+                    dataStr = entity.getIndustryCapUtil();
+                }
+                dataSerList.add(dataStr);
+            }
+
+            List<Series> series = new ArrayList<>();//纵坐标
+            series.add(new Series(typeName, "line",dataSerList));
+
+            EchartData data=new EchartData(legend, category, series);
+
+            return R.ok().put("data",data);
+        }
         return R.ok().put("data", null);
     }
 
--- src/main/java/io/renren/modules/pc/service/IndClassService.java
+++ src/main/java/io/renren/modules/pc/service/IndClassService.java
@@ -1,19 +1,28 @@
 package io.renren.modules.pc.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import io.renren.common.utils.R;
 import io.renren.modules.generator.entity.DljIndustryDataEntity;
+import io.renren.modules.generator.entity.DljIndustryEntity;
 import io.renren.modules.generator.service.DljIndustryDataService;
+import io.renren.modules.generator.service.DljIndustryService;
+import io.renren.modules.pc.entity.EchartData;
+import io.renren.modules.pc.entity.Series;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service("indClassService")
 public class IndClassService {
 
     @Autowired
     private DljIndustryDataService service;
+    @Autowired
+    private DljIndustryService industryService;
 
 
     public BigDecimal getTotalData(List<DljIndustryDataEntity> list){
@@ -37,6 +46,127 @@ public class IndClassService {
         if( null !=service.list(w)){
             return service.list(w);
         }
+        return null;
+    }
+
+
+
+    public EchartData getDisDataByMonth(Map<String, Object> params){
+        String  industryId = String.valueOf(params.get("industryId"));
+        String  type = String.valueOf(params.get("type"));
+
+
+        //通过行业id获取行业名称
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id",industryId);
+        List<DljIndustryEntity> list= industryService.list(wrapper);
+        DljIndustryEntity indEntity = list.get(0);
+
+        //通过行业去获取所有的list，然后获取到list拼接折线图的数据
+        QueryWrapper dataWrapper = new QueryWrapper();
+        dataWrapper.eq("industry_name",indEntity.getOverName());
+        List<DljIndustryDataEntity> dataList = service.list(dataWrapper);
+        if(dataList != null){
+            String typeName = "";
+
+            if(type.equals("1")){
+                typeName = "用户量";
+            }else if(type.equals("2")){
+                typeName = "装机容量";
+            }else if(type.equals("3")){
+                typeName = "电量";
+            }else{
+                typeName = "产能利用率";
+            }
+
+            //分组
+            List<String> legend = new ArrayList<String>();
+            legend.add(typeName);
+
+            List<String> category= new ArrayList<>();
+            List<String> dataSerList = new ArrayList<>();
+            for (DljIndustryDataEntity entity : dataList) {
+                category.add(entity.getRecordTime());
+                String dataStr = "";
+                if(type.equals("1")){
+                    dataStr = entity.getUserNum();
+                }else if(type.equals("2")){
+                    dataStr = entity.getInstalledCapacity();
+                }else if(type.equals("3")){
+                    dataStr = entity.getEleConMonth();
+                }else{
+                    dataStr = entity.getIndustryCapUtil();
+                }
+                dataSerList.add(dataStr);
+            }
+
+            List<Series> series = new ArrayList<>();//纵坐标
+            series.add(new Series(typeName, "line",dataSerList));
+
+            EchartData data=new EchartData(legend, category, series);
+            return data;
+        }
+
+        return null;
+    }
+
+
+
+    public EchartData getDisDataByYear(Map<String, Object> params){
+        String  industryId = String.valueOf(params.get("industryId"));
+        String  type = String.valueOf(params.get("type"));
+
+        //通过行业id获取行业名称
+        QueryWrapper wrapper = new QueryWrapper();
+        wrapper.eq("id",industryId);
+        List<DljIndustryEntity> list= industryService.list(wrapper);
+        DljIndustryEntity indEntity = list.get(0);
+
+        //通过行业去获取所有的list，然后获取到list拼接折线图的数据
+        QueryWrapper dataWrapper = new QueryWrapper();
+        dataWrapper.eq("industry_name",indEntity.getOverName());
+        List<DljIndustryDataEntity> dataList = service.list(dataWrapper);
+        if(dataList != null){
+            String typeName = "";
+
+            if(type.equals("1")){
+                typeName = "用户量";
+            }else if(type.equals("2")){
+                typeName = "装机容量";
+            }else if(type.equals("3")){
+                typeName = "电量";
+            }else{
+                typeName = "产能利用率";
+            }
+
+            //分组
+            List<String> legend = new ArrayList<String>();
+            legend.add(typeName);
+
+            List<String> category= new ArrayList<>();
+            List<String> dataSerList = new ArrayList<>();
+            for (DljIndustryDataEntity entity : dataList) {
+                category.add(entity.getRecordTime());
+                String dataStr = "";
+                if(type.equals("1")){
+                    dataStr = entity.getUserNum();
+                }else if(type.equals("2")){
+                    dataStr = entity.getInstalledCapacity();
+                }else if(type.equals("3")){
+                    dataStr = entity.getEleConMonth();
+                }else{
+                    dataStr = entity.getIndustryCapUtil();
+                }
+                dataSerList.add(dataStr);
+            }
+
+            List<Series> series = new ArrayList<>();//纵坐标
+            series.add(new Series(typeName, "line",dataSerList));
+
+            EchartData data=new EchartData(legend, category, series);
+            return data;
+        }
+
         return null;
     }
 }
